c18349a8de7a2300d1edc238dd2bb0be1ecab682
Aaron Harnly <aharnly@amplify.com>
2016-03-26 18:51:16 -0400

    Stub debate scrape script

---
+++ scripts/debates-scrape.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+Scrapes Presidential debates from the UCSB American Presidency Project.
+
+Creates YAML files named by date, with the raw text scraped from the site.
+Subsequent scripts or hand-munging are required to properly annotate the speakers.
+"""
+import sys
+
+INDEX_URL="http://www.presidency.ucsb.edu/debates.php"
+
+
+def main(args):
+    """
+    """
+
+
+if __name__ == '__main__':
+    main(sys.argv)
